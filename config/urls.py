eb67bb0f261f00f52a76437981a264fef61e6c77
Dev <fedorskarvinko@gmail.com>
2026-02-18 04:34:26 +0400

    add new htmls

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
+    path("catalog/", include("catalog.urls", namespace="catalog")),
+    path("blog/", include("blog.urls", namespace="blog")),
 ]
